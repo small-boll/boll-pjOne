a1133f2fdeb1d52273fb4391d18294ab2027bf52
liangmin <649554709@qq.com>
2022-08-23 13:21:42 +0800

    feat:3d新增动画

--- src/components/three/Three3D.vue
+++ src/components/three/Three3D.vue
@@ -17,12 +17,17 @@
 // 引入threejs核心模块
 import * as THREE from "three"
 export default {
-  name: 'PersonCenter',
+  name: 'Three3D',
   data () {
-    return {}
+    return {
+      timer: null,
+      scene: null,
+      renderer: null,
+      mesh: null,
+      camera: null
+    }
   },
   mounted () {
-    console.log('我是个人中心模块的main')
     this.$nextTick(
       this.init()
     )
@@ -30,27 +35,27 @@ export default {
   methods: {
     init () {
       // 创建场景对象Scene
-      const scene = new THREE.Scene();
+      this.scene = new THREE.Scene();
       /**
        * 创建网格模型
        */
-      // const geometry = new THREE.SphereGeometry(60, 40, 40); //创建一个球体几何对象
+      // const geometry = new THREE.SphereGeometry(60, 60, 60); //创建一个球体几何对象
       const geometry = new THREE.BoxGeometry(10, 100, 100); //创建一个立方体几何对象Geometry
       const material = new THREE.MeshLambertMaterial({
         color: 0x0000ff
       }); //材质对象Material
-      const mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
-      scene.add(mesh); //网格模型添加到场景中
+      this.mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
+      this.scene.add(this.mesh); //网格模型添加到场景中
       /**
        * 光源设置
        */
       //点光源
       const point = new THREE.PointLight(0xffffff);
       point.position.set(400, 200, 300); //点光源位置
-      scene.add(point); //点光源添加到场景中
+      this.scene.add(point); //点光源添加到场景中
       //环境光
       const ambient = new THREE.AmbientLight(0x444444);
-      scene.add(ambient);
+      this.scene.add(ambient);
       // console.log(scene)
       // console.log(scene.children)
       /**
@@ -61,20 +66,31 @@ export default {
       const k = width / height; //窗口宽高比
       const s = 200; //三维场景显示范围控制系数，系数越大，显示的范围越大
       //创建相机对象
-      const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
-      camera.position.set(200, 300, 200); //设置相机位置
-      camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+      this.camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+      this.camera.position.set(200, 300, 200); //设置相机位置
+      this.camera.lookAt(this.scene.position); //设置相机方向(指向的场景对象)
       /**
        * 创建渲染器对象
        */
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(width, height);//设置渲染区域尺寸
-      renderer.setClearColor(0xb9d3ff, 1); //设置背景颜色
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(width, height);//设置渲染区域尺寸
+      this.renderer.setClearColor(0xb9d3ff, 1); //设置背景颜色
       // document.body.appendChild(renderer.domElement); //body元素中插入canvas对象
-      document.getElementById('threeContain').appendChild(renderer.domElement)
+      document.getElementById('threeContain').appendChild(this.renderer.domElement)
       //执行渲染操作   指定场景、相机作为参数
-      renderer.render(scene, camera);
+      // renderer.render(this.scene, this.camera);
+      // this.timer = setInterval(this.render,20);
+      this.renderFn()
+    },
+    renderFn() {
+      this.renderer.render(this.scene,this.camera);//执行渲染操作
+      this.mesh.rotateY(0.01);//每次绕y轴旋转0.01弧度
+      // 调用一个函数不是立即调用而是向浏览器发起一个执行某函数的请求， 什么时候会执行由浏览器决定，一般默认保持60FPS的频率，大约每16.7ms调用一次requestAnimationFrame()方法指定的函数
+      requestAnimationFrame(this.renderFn);//请求再次执行渲染函数render
     }
+  },
+  beforeDestroy () {
+    clearInterval(this.timer);
   }
 }
 </script>
